e4c076d4d618ce35086170175041ac0f632286ec
kaliverda-denis <Your email>
2025-11-24 19:40:44 +0200

    f

---
+++ pract5/1.js
@@ -0,0 +1,10 @@
+function countVowels(str) {
+  const vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
